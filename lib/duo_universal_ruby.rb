72ab67799f7d0f81d7f9317df7ce6640d7549f8f
Todd Parsnick <todd.parsnick@ucsf.edu>
2025-07-28 16:30:15 -0700

    initial structure created by bundle gem

---
+++ lib/duo_universal_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "duo_universal_ruby/version"
+
+module DuoUniversalRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/duo_universal_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DuoUniversalRuby
+  VERSION = "0.1.0"
+end
